e4cded174b725c1769dba39d44b4592e775036f4
yash-sans2004 <nayan251198@gmail.com>
2025-01-05 17:22:16 +0530

    Initialize the project

---
+++ src/main/java/com/example/BookMyShow_Backend/BookMyShowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShow_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookMyShow_Backend/BookMyShowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShow_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
